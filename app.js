6daa944c1a352988edbe4753b2e12a6e636a3d82
Volodymyr_D <dymyr307@gmail.com>
2024-02-18 19:53:17 +0200

    start create async logic

---
+++ app.js
@@ -0,0 +1,32 @@
+//* Globals
+
+let todos = [];
+let users = [];
+
+//* Attach Events
+
+document.addEventListener('DOMContentLoaded', initApp);
+
+//* Event logic
+
+function initApp() {
+  Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
+    [todos, users] = values;
+  });
+}
+
+//* Async logic
+
+async function getAllTodos() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await response.json();
+
+  return data;
+}
+
+async function getAllUsers() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json();
+
+  return data;
+}
